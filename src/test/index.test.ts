71dff3c4221b424104882fa53f82b37177388810
r2k <r2kodepl@gmailcom>
2023-01-11 22:38:46 +0100

    chore: add vitest

---
+++ src/test/index.test.ts
@@ -0,0 +1,20 @@
+// exapmles from https://stackblitz.com/fork/github/vitest-dev/vitest/tree/main/examples/basic?initialPath=__vitest__
+import { assert, expect, test } from 'vitest';
+
+test('Math.sqrt()', () => {
+  expect(Math.sqrt(4)).toBe(2);
+  expect(Math.sqrt(144)).toBe(12);
+  expect(Math.sqrt(2)).toBe(Math.SQRT2);
+});
+
+test('JSON', () => {
+  const input = {
+    foo: 'hello',
+    bar: 'world',
+  };
+
+  const output = JSON.stringify(input);
+
+  expect(output).eq('{"foo":"hello","bar":"world"}');
+  assert.deepEqual(JSON.parse(output), input, 'matches original');
+});
