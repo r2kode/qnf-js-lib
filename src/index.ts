7971550a61b98dca000cda9c4f585d419976da9d
R2Kode <r2kodepl@gmail.com>
2023-03-19 12:26:44 +0100

    refactor: refactor exports (#28)
    Co-authored-by: r2k <r2kodepl@gmailcom>

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,5 @@
-export { relativeTime } from './Intl/relativeTime';
-export {
-  removeDuplicates,
-  minNumberInArray,
-  maxNumberInArray,
-  occurrenceCount,
-} from './utils/arrays.utils';
-export {
-  cloneObj,
-  cloneWithJson,
-  cloneWithGlobalMethod,
-} from './utils/objects.utils';
-export { midValue } from './utils/math.utils';
-export { getWords, getSentences } from './Intl/segmenter';
-export { formatCelsius, formatMinutes } from './Intl/units';
+export * from './utils';
+export * from './toolkit';
+export * from './Intl';
 export { validateEmail } from './validators/email';
 export { addMutationObserver } from './dom/mutationObserver';
-
-export * from './toolkit';
